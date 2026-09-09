9c1713ae439de2632a4c1bb43e9026afe0ecd40c
Hansen Yang <hansenyang@berkeley.edu>
2026-09-08 22:50:02 -0700

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        res = []
+        m = {}
+        n = int(len(nums) / 3)
+        for num in nums:
+            if num in m:
+                m[num] += 1
+                
+            else:
+                m[num] = 1
+            if m[num] > n + 1:
+                continue
+            elif m[num] > n:
+                res.append(num)
+
+
+        return res
