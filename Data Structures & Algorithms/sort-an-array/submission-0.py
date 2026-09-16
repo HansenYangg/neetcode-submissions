dbfa3c45153368930672eb4bc495c18b3d1a487a
Hansen Yang <hansenyang@berkeley.edu>
2026-09-15 20:12:15 -0700

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    import heapq
+    def sortArray(self, nums: List[int]) -> List[int]:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+        res = []
+        while heap:
+            res.append(heapq.heappop(heap))
+
+        return res
